d9668436b19e85da9232a9fe965ceeae31fef224
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-04 08:20:11 +0600

    mathematical operation

---
+++ 4-Milestone (Hello JavaScript)/17-Introduction to JavaScript/17-9 (advanced) Mathematical operation shorthand/main.js
@@ -0,0 +1,26 @@
+var price = 32;
+// price = price + 10
+price += 10;
+console.log(price);
+
+var fridge = 45000;
+// fridge = fridge -3000
+fridge -= 3000;
+console.log(fridge);
+
+
+var orangePrice = 30;
+// orangePrice = orangePrice * 10
+orangePrice *= 10;
+console.log(orangePrice);
+
+
+var first = 'mobarok';
+var second = 'tobarok';
+
+var fullName = first + second;
+console.log(fullName);
+
+console.log(20 + '10');
+console.log(20 - '10');
+console.log("20" * 10);
