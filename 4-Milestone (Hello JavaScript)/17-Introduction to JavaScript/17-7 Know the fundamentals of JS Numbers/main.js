5a7587669df803111d8827c44f14ef6737da213c
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-04 08:03:02 +0600

    fundamental of js number

---
+++ 4-Milestone (Hello JavaScript)/17-Introduction to JavaScript/17-7 Know the fundamentals of JS Numbers/main.js
@@ -0,0 +1,20 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+
+console.log(orangePrice);
+console.log(chocolate);
+console.log(orangePrice + chocolate);
+
+var applePrice = '43.5';
+console.log(orangePrice + orangePrice);
+var applePrice = parseFloat('43.5');
+console.log(orangePrice + applePrice);
+
+var num = parseInt('53');
+console.log(num + chocolate);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(2)); // toFixed() return a string
