0e339c79508cef1188a85653044aa9f1f2791a5f
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-04 07:27:54 +0600

    string and boolean

---
+++ 4-Milestone (Hello JavaScript)/17-Introduction to JavaScript/17.5-Variable data type, Numerich, String, Boolean/main.js
@@ -0,0 +1,11 @@
+// string
+var country = 'Bangladesh';
+console.log(country);
+console.log(typeof country);
+
+// boolean 
+var isRich = true;
+var passed = false;
+console.log(isRich);
+console.log(passed);
+console.log(typeof isRich, typeof passed);
